747227d47164e373bea450436f2a8ae7d3cd98ba
Santiago Gutierrez Cardona <104087575+Grafito94@users.noreply.github.com>
2022-09-19 17:12:14 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+#http://localhost:5000/
+@app.route('/')
+def inicio():
+    return 'hola mundo'
+
+if __name__== '__main__':
+    app.run(debug=True)
